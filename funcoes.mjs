0577f84b4fd56dca81c6e2018fa712bbf281e56f
Alana Barbosa <107224487+AlanaBarbosa1802@users.noreply.github.com>
2022-07-10 16:21:33 -0300

    Add files via upload

---
+++ funcoes.mjs
@@ -0,0 +1,13 @@
+function mostraIdade(nome, idade){
+    return 'A idade de ${nome} é $(idade)';    
+}
+
+function mostraCidade(nome, cidade){
+    return 'A Cidade de ${nome} é $(cidade)';    
+}
+
+function mostraMusicafavorita(nome, musica){
+    return 'A musica favorita de de ${nome} é $(musica)';    
+}
+
+export{mostraIdade, mostraCidade, mostraMusicafavorita};
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraIdade, mostraMusicafavorita} from ".funcoes.mjs"; from "./funcoes.mjs";
+
+console.log(mostraIdade("Alana", 31));
+console.log(mostraCidade("Alana", "São Paulo"));
+console.log(mostraMusicafavorita("Alana", "O sol"));
